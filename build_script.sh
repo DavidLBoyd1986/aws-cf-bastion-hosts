75884a93e8a586433c2bdee6dba5956a20d07b90
David <davidlboyd1986@gmail.com>
2024-12-19 13:37:27 -0500

    Fixed typo in IAM Role. Cleaned up where tags go in IAM Role. Changed stack names to remove ekshacking.

--- build_script.sh
+++ build_script.sh
@@ -7,16 +7,16 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 # Deploy the Bastion Host VPC and Infrastructure Stacks
 echo "Deploy the BH Networking stack"
-aws cloudformation deploy --stack-name eks-hacking-bh-vpc-stack \
+aws cloudformation deploy --stack-name bh-vpc-stack \
     --template-file $SCRIPT_DIR/IaC/bastion_host_vpc_deployment.yml
 
 echo "Deploy the BH IAM stack"
-aws cloudformation deploy --stack-name eks-hacking-bh-iam-stack \
+aws cloudformation deploy --stack-name bh-iam-stack \
     --template-file $SCRIPT_DIR/IaC/bastion_host_iam_deployment.yml \
     --capabilities CAPABILITY_IAM
 
 echo "Deploy the BH Infrastructure stack - EC2 Instance with Security Groups"
-aws cloudformation create-stack --stack-name eks-hacking-bh-infrastructure-stack \
+aws cloudformation create-stack --stack-name bh-infrastructure-stack \
     --template-body file://$SCRIPT_DIR/IaC/bastion_host_infrastructure_deployment.yml \
     --parameters file://$SCRIPT_DIR/parameters.json \
     --capabilities CAPABILITY_NAMED_IAM
